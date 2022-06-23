ba70d153242a764cc510f36b65775d75f3291e82
ShishiFoxy <shirley.bsth@gmail.com>
2022-06-23 09:01:44 +0200

    spring 02

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,32 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number ) {
+        if (number == 13) {
+            Doctor doctorTest = new Doctor();
+            doctorTest.setName("Jodie Whittaker");
+            doctorTest.setNumber(number);
+            return doctorTest;
+        }
+        if (number >=1 && number <=12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
 
-        return null;
     }
 }
